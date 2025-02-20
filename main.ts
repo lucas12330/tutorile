a41b9e7ae0717a1bc4c551f845e970d1c1d4b472
lucas12330 <ligueketoniejudo@gmail.com>
2025-02-20 14:20:03 +0100

    ajout du montrer led

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 let strip = 0
 basic.forever(function () {
+    basic.showLeds(`
+        # . . # #
+        . # # # #
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
     strip += 1
     basic.pause(1000)
 })
